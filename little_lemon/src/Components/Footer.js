077777d6b7bd96a5fb5fda2997ba343094395320
Muhammad Hamza <91649627+MuhammadHamza00@users.noreply.github.com>
2023-09-10 21:15:15 +0500

    Commit folder

---
+++ little_lemon/src/Components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import flogo from "../Assets/footerlogo.png"
+export default function Footer() {
+  return (
+    <footer>
+        <img src={flogo} alt="This is Logo"/>
+    </footer>
+  )
+}
---
+++ little_lemon/src/Components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <nav>
+        <ul>
+          <li><a>Home</a></li>
+          <li><a>About</a></li>
+          <li><a>Menu</a></li>
+          <li><a>Reservation</a></li>
+          <li><a>Order Line</a></li>
+          <li><a>Login</a></li>
+        </ul>
+    </nav>
+  )
+}
